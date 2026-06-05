9e024e179e85c45222ef6cd081263bebbaae5cab
Mariano Reynaga Ducos <mreynagaducos@icloud.com>
2026-06-04 22:41:57 -0300

    feat(js): implementar manejo de ruta no existente Closes #6

--- src/server.js
+++ src/server.js
@@ -35,5 +35,8 @@ app.get('/usuarios/:id', (req, res) => {
   res.json(usuario);
 });
 
+app.use((req, res) => {
+  res.status(404).send('No se ha encontrado la ruta ingresada.');
+});
 
 module.exports = app;
